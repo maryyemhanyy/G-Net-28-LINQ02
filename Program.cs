ea6194bf100b5f3122a2a41f059b8f442161be8c
maryem <maryyemhanyy@gmail.com>
2026-04-07 03:19:43 +0200

    Q13

--- Program.cs
+++ Program.cs
@@ -151,6 +151,22 @@ namespace LINQSession2
             //    Console.WriteLine(p);
             //}
             #endregion
+
+            #region Q13
+            /*find product IDs that are in setA but NOT in setB 
+              int[] setA = { 1, 3, 5, 7, 9, 11, 13 };      
+              int[] setB = { 3, 6, 9, 12, 15, 13 }; */
+
+            //int[] setA = { 1, 3, 5, 7, 9, 11, 13 };
+            //int[] setB = { 3, 6, 9, 12, 15, 13 };
+
+            //var result = setA.Except(setB);
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
         }
     }
 }
