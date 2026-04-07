ac189bdf1265e7cb0b9ef50b991ed1947f6cb80b
maryem <maryyemhanyy@gmail.com>
2026-04-07 02:47:27 +0200

    Q1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            //Get top 3 most expensive products 
+            //var result = Source.ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+
+            //foreach (var p in result)
+            //{
+            //    Console.WriteLine(p);
+            //}
+
+            #endregion
         }
     }
 }
