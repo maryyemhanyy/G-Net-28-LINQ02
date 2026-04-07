6748f2d40c3c9a53ce3280d2266c45f058243a93
maryem <maryyemhanyy@gmail.com>
2026-04-07 03:21:57 +0200

    Q14

--- Program.cs
+++ Program.cs
@@ -167,6 +167,22 @@ namespace LINQSession2
             //    Console.WriteLine(item);
             //}
             #endregion
+
+            #region Q14
+            /*Find countries that  appear in list1 but NOT in list2 (case-insensitive). 
+              string[] list1 = { "Germany", "France", "UK", "Spain" }; 
+              string[] list2 = { "france", "SPAIN", "Italy" }; */
+
+            //string[] list1 = { "Germany", "France", "UK", "Spain" };
+            //string[] list2 = { "france", "SPAIN", "Italy" };
+
+            //var result = list1.Except(list2, StringComparer.OrdinalIgnoreCase);
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
         }
     }
 }
